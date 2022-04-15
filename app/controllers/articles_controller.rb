95116b9116882ec76541b925744a8974d9ee6bba
Peter <poxley@alumni.nnu.edu>
2022-04-15 08:27:12 -0600

    part 11 complete - basic security

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,7 @@
 class ArticlesController < ApplicationController
+
+  http_basic_authenticate_with name: "poxley", password: "notSoFast", except: [:index, :show]
+
   def index
     @articles = Article.all
   end
